9d3b237cf7051b7da20ddb33efeae91c73818749
mahxmoud <mahmoud.jomaa@yahoo.com>
2021-01-06 06:41:28 +0200

    Initiated the store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import VisibilityFilters from "./components/VisibilityFilters";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <h1>Todo List</h1>
+      <AddTodo />
+      <TodoList />
+      <VisibilityFilters />
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import TodoApp from "./App";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
-
