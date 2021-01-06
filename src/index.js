6dbe0c3f58497c0dfc200d4fbe61dc135779cb0c
mahxmoud <mahmoud.jomaa@yahoo.com>
2021-01-06 07:13:53 +0200

    fixed some typos

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-import TodoApp from "./App";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
